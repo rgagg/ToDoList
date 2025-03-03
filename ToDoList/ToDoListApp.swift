1eef650bc79e92114b96130059f20f3654a53483
Richard Gagg <r.gagg@outlook.com>
2025-03-03 14:48:02 +1100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Richard Gagg on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
